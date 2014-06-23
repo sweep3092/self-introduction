d19d2eed5d7f0468b81e95c5f6c5d66e8de78fd1
You Kobayashi <sweep.3092@gmail.com>
2014-06-23 16:09:49 +0900

    あきらめた

--- js/mileage.js
+++ js/mileage.js
@@ -1,26 +1,16 @@
-function showMap() {
+var location_geocode;
+
+function show_map() {
     $("#map-canvas").show().addClass("block-shadow");
 }
 
-// Google Maps API マップ作成
-function initMaps(latlng) {
-    var opts = {
-        zoom: 15,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), opts);
-    var marker = new google.maps.Marker( { position: latlng, map: map} );
-
-    showMap();
-}
-
-// Google Maps API geocoder & initMaps
-function geoCoding(place_name) {
+// Google Maps API geocoder
+function find_geocode(place_name) {
     var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({address:place_name}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            initMaps(results[0].geometry.location);
+    geocoder.geocode( { address: place_name }, function(result, status) {
+        if ( status == google.maps.GeocoderStatus.OK ) {
+            location_geocode = result[0].geometry.location;
+            draw_map( location_geocode );
         }
         else {
             alert("指定されたポイントの検索に失敗");
@@ -28,8 +18,26 @@ function geoCoding(place_name) {
     });
 }
 
+// Google Maps API
+function draw_map(geocode) {
+    if (!geocode) return false;
+
+    var opts = {
+        zoom: 15,
+        center: geocode,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), opts);
+    var marker = new google.maps.Marker( {
+        position: geocode,
+        map: map
+    } );
+
+    show_map();
+}
+
 // Google Maps API route-Request
-function showRoute(departure, destination) {
+function show_route(departure, destination) {
     var directionsDisplay = new google.maps.DirectionsRenderer();
     var directionsService = new google.maps.DirectionsService();
 
@@ -52,12 +60,12 @@ function showRoute(departure, destination) {
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
             $("#distance").val(response.routes[0].legs[0].distance.value);
+            show_map();
         }
         else {
             alert("ルートの探索に失敗");
         }
     });
-    showMap();
 }
 
 $(document).ready(function() {
@@ -65,16 +73,35 @@ $(document).ready(function() {
         e.preventDefault();
         var efficiency  = $("#efficiency").val();
         var distance    = $("#distance").val();
+        var oiltype     = $("#oiltype").val();
+        console.log(location_geocode);
+
+        $.ajax({
+            url : "http://api.gogo.gs/v1.2/",
+            crossDomain: true,
+            data : {
+                apid : "mileagekdjfaoirj",
+                num   : 1,
+                kind  : oiltype,
+                lat   : location_geocode.lat(),
+                lon   : location_geocode.lng()
+            },
+            type : "get",
+            dataType: "xml",
+            success : function(xml) {
+                console.log( $(xml.responseText) );
+            }
+        });
     });
 
     var search_place = function(place_a, place_b) {
         // 2文字以上なら
-        if (place_a.length > 1) {
-            if (place_b) {
-                showRoute(place_a, place_b);
+        if ( place_a.length > 1 ) {
+            if ( place_b ) {
+                show_route( place_a, place_b );
             }
             else {
-                geoCoding(place_a);
+                find_geocode( place_a );
             }
         }
     }
