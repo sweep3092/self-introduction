a3e177f0f980a14ecaf496123b8ef488eebff251
You Kobayashi <sweep.3092@gmail.com>
2014-10-06 01:00:40 +0900

    noto sans

--- mileage.php
+++ mileage.php
@@ -11,14 +11,14 @@ $res_geo   = simplexml_load_file($req_geo);
 
 // API gogo.gs
 $param_gas = "&kind=".htmlspecialchars($_GET['oiltype'], ENT_QUOTES, 'UTF-8')."&lat=".$res_geo->result->geometry->location->lat."&lon=".$res_geo->result->geometry->location->lng;
-$req_gas   = "http://api.gogo.gs/v1.2/?apid=mileagekdjfaoirj&num=1".$param_gas;
+$req_gas   = "http://api.gogo.gs/v1.2/?apid=mileagekdjfaoirj&num=1&dist=3".$param_gas;
 $res_gas   = simplexml_load_file($req_gas);
 
-$cost = $_GET['distance'] / $_GET['efficiency'] * $res_gas->Item->Price * 0.001;
+$cost = $distance / $efficiency * $res_gas->Item->Price * 0.001;
 
 $output = array(
-    'price'    => $res_gas->Item->Price,
-    'shopcode' => $res_gas->Item->ShopCode,
+    'price'    => intval( $res_gas->Item->Price ),
+    'shopcode' => intval( $res_gas->Item->ShopCode ),
     'cost'     => floor( $cost ),
 );
 
